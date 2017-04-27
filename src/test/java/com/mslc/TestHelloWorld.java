305bf8dcd9e577e116e608becb70f51f8bf36c83
mani0707 <mani.it@gmail.com>
2017-04-27 14:56:45 +0530

    Update TestHelloWorld.java

--- src/test/java/com/mslc/TestHelloWorld.java
+++ src/test/java/com/mslc/TestHelloWorld.java
@@ -27,6 +27,6 @@ public class TestHelloWorld {
 	   {
 	      h.setName("World");
 	      assertEquals(h.getName(),"World");
-	      assertEquals(h.getMessage(),"Hello World! *** MANI ***");
+	      assertEquals(h.getMessage(),"Hello World!");
 	   }
 }
